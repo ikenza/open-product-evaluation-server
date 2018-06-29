98467958358dfa42082f1fd7e175537b5c3d7b04
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-06-30 00:57:51 +0200

    cleaned up setup function

--- test/setup.js
+++ test/setup.js
@@ -1,13 +1,7 @@
 const { seedDatabase } = require('mongo-seeding')
 const config = require('../config')
 
-console.log('-----------------Preparing Test-----------------')
-
 
 beforeEach(async () => {
-  console.log('seeding db...')
   await seedDatabase(config.seeder)
 })
-
-
-console.log('---------------Preparing Finished---------------')
