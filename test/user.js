fd1f9eadf9f5a78b2f801ea94f7702e6feb36274
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2018-06-30 00:58:16 +0200

    defined more tests for user

--- test/user.js
+++ test/user.js
@@ -7,38 +7,44 @@ const request = require('supertest')(url)
 
 chai.should()
 
-
-// TODO seed before test mocha global before index
-
 describe('User Resolver', () => {
-  describe('Querys', () => {
-    describe('User is not authed', () => {
-      it('should throw an Error', async () => {
-        const query = {
-          query: `mutation {
-            login(data: {email: "Jake@doe.com", password: "fe605d3cac6d5698bd85e76ebfbdee18763519c7"}) {
-              token
-              user {
-                id
-                creationDate
-                lastUpdate
-                firstName
-                lastName
-                email
-              }
+  describe.skip('Querys', () => {
+    it('should return a list of all Users')
+    it('should return a specific User')
+  })
+  describe('Mutations', async () => {
+    it('should create a User')
+    it('should update a User')
+    it('should delete a User')
+    it('should deny login of an invalid User')
+    it('should login a valid User', async () => {
+      const query = {
+        query: `mutation {
+          login(data: {email: "Jake@doe.com", password: "fe605d3cac6d5698bd85e76ebfbdee18763519c7"}) {
+            token
+            user {
+              id
+              creationDate
+              lastUpdate
+              firstName
+              lastName
+              email
             }
-          }`,
-        }
+          }
+        }`,
+      }
 
-        const expect = users[2]
+      const expect = users[2]
 
-        const res = await request.post('/')
-          .set('Accept', 'application/json')
-          .send(query)
-        const { body: { data } } = res
-        data.login.token.should.be.a('string')
-        data.login.user.should.be.deep.equal(expect)
-      })
+      const res = await request.post('/')
+        .set('Accept', 'application/json')
+        .send(query)
+      const { body: { data } } = res
+      const { login: { user } } = data
+      data.login.token.should.be.a('string')
+      user.firstName.should.be.equal(expect.firstName)
+      user.lastName.should.be.equal(expect.lastName)
+      user.email.should.be.equal(expect.email)
     })
   })
 })
